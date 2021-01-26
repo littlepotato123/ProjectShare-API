71f960a40fa26c15966b0c1fc76d3ba317f25ba1
littlepotato123 <sopanda0910@gmail.com>
2021-01-26 04:43:06 -0500

    Category Fix

--- dist/resolvers/Category/CategoryResolver.js
+++ dist/resolvers/Category/CategoryResolver.js
@@ -42,7 +42,20 @@ CreateCategoryInput = __decorate([
 let CategoryResolver = class CategoryResolver extends typeorm_1.BaseEntity {
     category_init() {
         return __awaiter(this, void 0, void 0, function* () {
-            return false;
+            const categories = yield Category_1.Category.find();
+            if (categories.length > 0) {
+                return false;
+            }
+            else {
+                yield Category_1.Category.create({ title: "Racism", description: "This is a category where anything that you feel is discriminating by race in politics, cities, or even your own job. You can feel free to be as detailed as you would like!" }).save();
+                yield Category_1.Category.create({ title: "Politics", description: "This is a category where you can post to whenever you find news that you feel the need to share about. Whethere it is a national update, or a small city construction update, if you feel the need to put it online, put it on Project Sh@re!" }).save();
+                yield Category_1.Category.create({ title: "Issues", description: "This is a category where you can post anything about any issues that you are facing, you see others facing, or even the nation or world facing. This can be as little as a small manifacture mistake, all the way to a global pandemic." }).save();
+                yield Category_1.Category.create({ title: "Campaigning", description: "Feel free to post anything that promotes your channel, accounts, or political status on Project Sh@are using this category!" }).save();
+                yield Category_1.Category.create({ title: "Introduction", description: "Project Sh@re is a online world, just like any other, and usually it helps to introduce yourself before trying to get famous or even post anything online. In this category, you can post anything about yourself or anything you feel the need to share with others about yourself!" }).save();
+                yield Category_1.Category.create({ title: "Anything", description: "This category is for really anything. Mainly used for comments and user feedback, many authors use this category to let their supporters talk to them and let them know what they would like to hear from the author." }).save();
+                yield Category_1.Category.create({ title: "Economics", description: "This category is for any new business or economic spikes or downfalls. You can use this category to update the users of Project Sh@re on what is going on in the world of economics, stocks, trade market, and finance as a whole!" }).save();
+                return true;
+            }
         });
     }
     ;
--- src/resolvers/Category/CategoryResolver.ts
+++ src/resolvers/Category/CategoryResolver.ts
@@ -16,20 +16,20 @@ class CreateCategoryInput {
 export class CategoryResolver extends BaseEntity {
     @Mutation(() => Boolean) 
     async category_init() {
-        // const categories = await Category.find();
-        // if(categories.length > 0) {
-        //     return false;
-        // } else {
-        //     await Category.create({ title: "Racism", description: "This is a category where anything that you feel is discriminating by race in politics, cities, or even your own job. You can feel free to be as detailed as you would like!" }).save();
-        //     await Category.create({ title: "Politics", description: "This is a category where you can post to whenever you find news that you feel the need to share about. Whethere it is a national update, or a small city construction update, if you feel the need to put it online, put it on Project Sh@re!" }).save();
-        //     await Category.create({ title: "Issues", description: "This is a category where you can post anything about any issues that you are facing, you see others facing, or even the nation or world facing. This can be as little as a small manifacture mistake, all the way to a global pandemic." }).save();
-        //     await Category.create({ title:"Campaigning", description: "Feel free to post anything that promotes your channel, accounts, or political status on Project Sh@are using this category!" }).save();
-        //     await Category.create({ title: "Introduction", description: "Project Sh@re is a online world, just like any other, and usually it helps to introduce yourself before trying to get famous or even post anything online. In this category, you can post anything about yourself or anything you feel the need to share with others about yourself!" }).save()
-        //     await Category.create({ title: "Anything", description: "This category is for really anything. Mainly used for comments and user feedback, many authors use this category to let their supporters talk to them and let them know what they would like to hear from the author." }).save();
-        //     await Category.create({ title: "Economics", description: "This category is for any new business or economic spikes or downfalls. You can use this category to update the users of Project Sh@re on what is going on in the world of economics, stocks, trade market, and finance as a whole!" }).save();
-        //     return true;
-        // }
-        return false;
+        const categories = await Category.find();
+        // Testing
+        if(categories.length > 0) {
+            return false;
+        } else {
+            await Category.create({ title: "Racism", description: "This is a category where anything that you feel is discriminating by race in politics, cities, or even your own job. You can feel free to be as detailed as you would like!" }).save();
+            await Category.create({ title: "Politics", description: "This is a category where you can post to whenever you find news that you feel the need to share about. Whethere it is a national update, or a small city construction update, if you feel the need to put it online, put it on Project Sh@re!" }).save();
+            await Category.create({ title: "Issues", description: "This is a category where you can post anything about any issues that you are facing, you see others facing, or even the nation or world facing. This can be as little as a small manifacture mistake, all the way to a global pandemic." }).save();
+            await Category.create({ title:"Campaigning", description: "Feel free to post anything that promotes your channel, accounts, or political status on Project Sh@are using this category!" }).save();
+            await Category.create({ title: "Introduction", description: "Project Sh@re is a online world, just like any other, and usually it helps to introduce yourself before trying to get famous or even post anything online. In this category, you can post anything about yourself or anything you feel the need to share with others about yourself!" }).save()
+            await Category.create({ title: "Anything", description: "This category is for really anything. Mainly used for comments and user feedback, many authors use this category to let their supporters talk to them and let them know what they would like to hear from the author." }).save();
+            await Category.create({ title: "Economics", description: "This category is for any new business or economic spikes or downfalls. You can use this category to update the users of Project Sh@re on what is going on in the world of economics, stocks, trade market, and finance as a whole!" }).save();
+            return true;
+        }
     };
 
     @Query(() => [Category], { nullable: true })
